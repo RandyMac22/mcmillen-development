5c5794280571c04d1fafab6d701022868773ac9d
Randy McMillen <84250923+RandyMac22@users.noreply.github.com>
2022-02-21 12:15:38 -0600

    last update

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,7 +1,7 @@
-
+import underConstruction from "./img/underConstruction1.jpg"
 
 export default function Projects() {
-    return(
-        <img src="https://cdn4.vectorstock.com/i/1000x1000/82/18/website-construction-construct-under-development-vector-9078218.jpg" />
+    return( 
+        <img classname="construction" src={underConstruction} />
     )
 }
