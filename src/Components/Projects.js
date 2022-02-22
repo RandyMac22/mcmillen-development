f36a1f0f74e4b2323498555c42644dd2eccef2d4
Randy McMillen <84250923+RandyMac22@users.noreply.github.com>
2022-02-22 11:29:47 -0600

    update 11:30am 2/22/22

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,6 +2,9 @@ import underConstruction from "./img/maintenance-2422169_960_720.jpg";
 
 export default function Projects() {
     return( 
-        <img classname="projects" src={underConstruction} />
+        <div className="projects">
+            <img src={underConstruction} />
+        </div>
+        
     )
 }   
